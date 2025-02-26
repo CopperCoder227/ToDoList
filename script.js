d30ff34f6feed868be0b30002226920a0fb8c21d
CopperCoder227 <526csteiner@frhsd.com>
2025-02-26 11:48:06 -0500

    killgore says finish later

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 let tasks = [];
-document.getElementById("addTaskBtn").addEventListener("click", function () {
+document.getElementById("addTaskButton").addEventListener("click", function () {
   let taskInput = document.getElementById("taskInput").value;
 
   if (taskInput) {
+    tasks.push(taskInput);
+
+    document.getElementById("taskInput").value = "";
+
+    displayTasks();
   }
+  console.log(tasks);
 });
+
+function displayTasks() {
+  let taskList = document.getElementById("taskList");
+
+  taskList.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    let li = document.createElement("li");
+
+    li.classList.add(
+      "list-group-item",
+      "d-flex",
+      "justify-content-between",
+      "align-items-center"
+    );
+
+    li.innerHTML = `${task} <button class='btn btn-dark btn-sm' onclick='removeTask(${index})'> √ </button>`;
+
+    taskList.appendChild(li);
+  });
+}
